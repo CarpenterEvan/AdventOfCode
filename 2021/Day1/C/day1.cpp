321a9dc59f23d75ea786176afd2738cdb82a328c
Evan <evan.carpenter14@gmail.com>
2025-09-03 22:04:03 -0400

    Day 1 part 1 in C++

--- 2021/Day1/C/day1.cpp
+++ 2021/Day1/C/day1.cpp
@@ -1,3 +1,57 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+
+
+
+
+
+
+
+int main() {
+
+	FILE *filepointer;
+	
+	filepointer = fopen("/Users/evan/Programming/AdventOfCode/2021/Day1/input.txt", "r");
+
+	char myString[100];
+	char line[2][sizeof(myString)];
+
+	int current = 0;
+	int previous = -1;
+
+	int depth_counter = 0;
+
+	if (filepointer == NULL) {
+		
+		printf("Error opening file!\n");
+		
+		return 1;
+	} else {
+		
+		// Read the content and print it
+		while(fgets(line[current], sizeof(myString), filepointer) != NULL) {
+			
+			int curr_number;
+			int prev_number;
+
+			sscanf(line[current], "%d", &curr_number);
+			sscanf(line[previous], "%d", &prev_number);
+
+			if ( previous >=0 and curr_number > prev_number) {
+				//printf("%.3s -> %.3s increased\n", line[previous], line[current]);
+				depth_counter++;
+			} 
+			//if (previous >= 0){
+			//	printf("%.3s -> %.3s\n", line[previous], line[current]);
+			//}
+
+			previous = current;
+			current = 1 - current; 
+
+		}
+	}
+	printf("Depth increased [[ %d ]] times\n", depth_counter);
+	fclose(filepointer);
+	return 0;
+}
--- 2021/Day1/Python/day1.py
+++ 2021/Day1/Python/day1.py
@@ -1,3 +1,10 @@
-with open("input.txt",'r') as f:
+with open("/Users/evan/Programming/AdventOfCode/2021/Day1/input.txt",'r') as f:
 	lines = f.readlines()
-print(lines)
+
+counter = 0
+
+for index in range(len(lines)):
+	if lines[index] > lines[index-1] and index > 0:
+		counter += 1 
+
+print(counter)
